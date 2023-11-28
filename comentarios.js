603956280e1b507d6a65c3498e041bdbb408cc28
Fpxx777 <renato.fox.castillo@alumnos.sip.cl>
2023-11-28 12:20:16 -0300

    me aburri, pa la casa

--- comentarios.js
+++ comentarios.js
@@ -3,3 +3,23 @@ function publicar(){
     document.getElementById("all-coments"). innerHTML += `<h3>${temp}</h3>`
     document.getElementById("a").value = ""
 }
+function star(element){
+    var content = document.getElementById("start-content");
+    var noSelectStar = '<button onclick="star(this)" class="star">✰</button>';
+    var startSelect = '<button onclick="star(this)" class="star">★</button>';
+    if(element == content.children[0]){
+        content.innerHTML = startSelect + noSelectStar + noSelectStar + noSelectStar + noSelectStar;
+    }
+    if(element == content.children[1]){
+        content.innerHTML = startSelect + startSelect + noSelectStar + noSelectStar + noSelectStar;
+    }
+    if(element == content.children[2]){
+        content.innerHTML = startSelect + startSelect + startSelect + noSelectStar + noSelectStar;
+    }
+    if(element == content.children[3]){
+        content.innerHTML = startSelect + startSelect + startSelect + startSelect + noSelectStar;
+    }
+    if(element == content.children[4]){
+        content.innerHTML = startSelect + startSelect + startSelect + startSelect + startSelect;
+    }
+}
